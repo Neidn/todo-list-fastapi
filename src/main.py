51f3dd1a1ebcecf15dab33d8e4d83724462d8c73
Neidn <dty3152@gmail.com>
2024-02-13 14:41:47 +0900

    add CORS for localhost

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ main.py
 """
 from fastapi.applications import FastAPI
 from fastapi.exceptions import RequestValidationError
+from fastapi.middleware.cors import CORSMiddleware
 from starlette.exceptions import HTTPException
 from .core.exception_handlers import http_exception_handler, validation_exception_handler
 
@@ -10,6 +11,21 @@ from .core.exception_handlers import http_exception_handler, validation_exceptio
 def create_app() -> FastAPI:
     """ app factory method """
     app = FastAPI()
+
+    origins = [
+        "http://localhost",
+        "http://localhost:8000",
+        "http://localhost:8080",
+        "http://localhost:3000",
+    ]
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=origins,
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
 
     app.add_exception_handler(
         RequestValidationError,
