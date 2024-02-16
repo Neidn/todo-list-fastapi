ab7e1aeb2bf6ed8824862f24d6d946f67ea948fb
Neidn <dty3152@gmail.com>
2024-02-16 10:36:10 +0900

    modify model default value

--- src/apps/todo/models/schema/todos.py
+++ src/apps/todo/models/schema/todos.py
@@ -5,21 +5,21 @@ from pydantic import BaseModel
 class TodoCreateRequest(BaseModel):
     """ TodoItem Create request schema """
     title: str
-    content: Optional[str]
-    is_done: bool = False
+    content: Optional[str] = None
+    is_done: Optional[bool] = False
 
 
 class TodoUpdateRequest(BaseModel):
     """ TodoItem Update request schema """
-    title: Optional[str]
-    content: Optional[str]
-    is_done: Optional[bool]
+    title: Optional[str] = None
+    content: Optional[str] = None
+    is_done: Optional[bool] = False
 
 
 class TodoCreateSuccessResponse(BaseModel):
     """ TodoItem Create response schema """
     title: str
-    content: Optional[str]
+    content: Optional[str] = None
     created_at: str
 
 
@@ -27,7 +27,7 @@ class TodoGetResponse(BaseModel):
     """ TodoItem Get response schema """
     id: str
     title: str
-    content: Optional[str]
+    content: Optional[str] = None
     is_done: bool
     created_at: str
     updated_at: str
